8b485f10a25e74ba30e3f8dd7c10efcdb65764f8
hayeonlee12 <54343481+hayeonlee12@users.noreply.github.com>
2023-03-20 14:01:43 +0900

    mvc 패턴

---
+++ workspace/day05/src/main/java/com/codingbox/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.codingbox.dao;
+
+import com.codingbox.vo.RegitVo;
+
+public class UserDao {
+	public boolean join(RegitVo vo) {
+		// DB Connection
+		// insert
+		// ...
+		
+		return true;
+	}
+}
---
+++ workspace/day05/src/main/java/com/codingbox/vo/User.java
@@ -0,0 +1,13 @@
+package com.codingbox.vo;
+
+public class User {
+	public String userid;
+	public String username;
+	public int age;
+	
+	public User(String userid, String username, int age) {
+		this.userid = userid;
+		this.username = username;
+		this.age = age;
+	}
+}
